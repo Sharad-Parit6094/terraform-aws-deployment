40a7b16cc89fc5d854e28cbc2e09c13fadcd6f06
Sharad Parit <sharadparit9642@gmail.com>
2025-12-08 10:53:49 +0530

    Create alb.tf

---
+++ terraform/alb.tf
@@ -0,0 +1,41 @@
+resource "aws_lb" "alb" {
+  name               = "${var.project}-${var.environment}-alb"
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = aws_subnet.public[*].id
+  tags = { Name = "${var.project}-${var.environment}-alb" }
+}
+
+# HTTP listener redirects to HTTPS
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+# HTTPS listener - certificate_arn variable is required
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.certificate_arn
+
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Not Found"
+      status_code  = "404"
+    }
+  }
+}
